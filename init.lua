22e874669e15d86e8f30022a4c8a942241712240
trevor.winn <trevor.winn@acacceptance.com>
2025-03-17 11:27:06 -0400

    Added keymap for neotree

--- init.lua
+++ init.lua
@@ -251,6 +251,9 @@ require('lazy').setup({
       'MunifTanjim/nui.nvim',
       -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    config = function()
+      vim.keymap.set('n', '<leader>wt', '<cmd>Neotree<cr>', { desc = 'Open Neotree' })
+    end,
   },
   { 'nvimtools/none-ls.nvim', lazy = true },
   {
