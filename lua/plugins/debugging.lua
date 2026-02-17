852a978a2caaf71a0e06f4fe136e01929d3a14cf
ClumsyBuddy <winnboys1105@gmail.com>
2026-02-16 20:44:50 -0500

    Fixed deprecated warning

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -20,7 +20,6 @@ return {
       local ui = require 'dapui'
       require('dapui').setup()
       require('dap-python').setup()
-      require('dap.ext.vscode').load_launchjs 'launch.json'
 
       require('nvim-dap-virtual-text').setup {
         -- Hides tokens, secrets, and other sensitive information
