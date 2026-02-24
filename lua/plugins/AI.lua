522797975f8eab76300f37a8fb57a78024ae5020
trevor.winn <trevor.winn@acacceptance.com>
2026-02-24 13:32:32 -0500

    Removed wtf

--- lua/plugins/AI.lua
+++ lua/plugins/AI.lua
@@ -30,67 +30,6 @@ return {
     end,
   },
   {
-    'piersolenski/wtf.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'MunifTanjim/nui.nvim',
-      'nvim-telescope/telescope.nvim',
-      'folke/snacks.nvim',
-      'ibhagwan/fzf-lua',
-    },
-    opts = {},
-    keys = {
-      {
-        '<leader>wwd',
-        mode = { 'n', 'x' },
-        function()
-          require('wtf').diagnose()
-        end,
-        desc = 'Debug diagnostic with AI',
-      },
-      {
-        '<leader>wwf',
-        mode = { 'n', 'x' },
-        function()
-          require('wtf').fix()
-        end,
-        desc = 'Fix diagnostic with AI',
-      },
-      {
-        mode = { 'n' },
-        '<leader>wws',
-        function()
-          require('wtf').search()
-        end,
-        desc = 'Search diagnostic with Google',
-      },
-      {
-        mode = { 'n' },
-        '<leader>wwp',
-        function()
-          require('wtf').pick_provider()
-        end,
-        desc = 'Pick provider',
-      },
-      {
-        mode = { 'n' },
-        '<leader>wwh',
-        function()
-          require('wtf').history()
-        end,
-        desc = 'Populate the quickfix list with previous chat history',
-      },
-      {
-        mode = { 'n' },
-        '<leader>wwg',
-        function()
-          require('wtf').grep_history()
-        end,
-        desc = 'Grep previous chat history with Telescope',
-      },
-    },
-  },
-  {
     "ThePrimeagen/99",
     config = function()
       local _99 = require("99")
