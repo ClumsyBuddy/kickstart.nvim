380e6a01f521745c70309f9d348522402cb4d44f
ClumsyBuddy <winnboys1105@gmail.com>
2026-02-27 17:24:57 -0500

    Fixed wilder

--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -2,25 +2,38 @@ return {
   {
     'gelguy/wilder.nvim',
     event = 'CmdlineEnter',
-    dependencies = { 'romgrk/fzy-lua-native' },
+    dependencies = {
+      { 'romgrk/fzy-lua-native', build = 'make', cond = vim.fn.has('win32') == 0 },
+    },
     config = function()
       local wilder = require('wilder')
       wilder.setup({ modes = { ':', '/', '?' } })
 
-      -- pipeline: fuzzy for cmdline, search pipeline for searches
+      -- Detect if native fuzzy is available (fails on Windows or if not compiled)
+      local has_fzy, _ = pcall(require, 'fzy-lua-native')
+
+      local fuzzy_filter = has_fzy
+        and wilder.lua_fzy_filter()
+        or wilder.vim_fuzzy_filter()
+
+      local highlighter = has_fzy
+        and wilder.lua_fzy_highlighter()
+        or wilder.basic_highlighter()
+
+      -- Pure Lua pipeline (no Python dependency)
       wilder.set_option('pipeline', {
         wilder.branch(
           wilder.cmdline_pipeline({
+            language = 'lua',
             fuzzy = 1,
-            fuzzy_filter = wilder.lua_fzy_filter(),
+            fuzzy_filter = fuzzy_filter,
           }),
           wilder.search_pipeline()
         ),
       })
 
-      -- simple popupmenu renderer
       wilder.set_option('renderer', wilder.popupmenu_renderer({
-        highlighter = wilder.basic_highlighter(),
+        highlighter = highlighter,
         left = { ' ', wilder.popupmenu_devicons() },
         right = { ' ', wilder.popupmenu_scrollbar() },
       }))
