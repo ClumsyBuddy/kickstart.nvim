6d3350f1412e55b89732d1204fbb5dcd88fb234e
trevor.winn <trevor.winn@acacceptance.com>
2026-02-27 12:44:52 -0500

    Removed ^M from lineendings

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -452,7 +452,7 @@ wk.add({
 
   { '<leader>g', group = '[g]it' },
   { '<leader>gc', ':GitConflictRefresh<cr>', desc = '[c]onflict' },
-  { '<leader>gs', ':Gitsigns<cr>', desc = 'git [s]igns' },
+  -- { '<leader>gs', ':Gitsigns<cr>', desc = 'git [s]igns' },
   { '<leader>gm', function() require('misc.ollama-commit').commit() end, desc = 'AI commit [m]essage' },
   { '<leader>gM', function() require('misc.ollama-commit').select_model() end, desc = 'Set Ollama [M]odel' },
   -- lazygit keymaps are in snacks.lua: <leader>gg, <leader>gf, <leader>gl
